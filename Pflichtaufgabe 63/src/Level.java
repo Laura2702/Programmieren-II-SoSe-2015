5b896ae84d79b26c9e3099b7c9fb523f11333943
Loreilya <sophie1996@gmx.de>
2015-06-05 14:40:57 +0200

    Level&Map.java
    neue Level und Map.java Datei

--- Pflichtaufgabe 63/src/Level.java
+++ Pflichtaufgabe 63/src/Level.java
@@ -1,435 +1,151 @@
-import java.util.Scanner;
-
 /**
- * The type Level.
- *
- * @author Max Mustermann 1234567 Gruppe 42z
- * @author Erika Musterfrau 1234567 Gruppe 42z
- */
+ *  Die Klasse Level erzeugt ein Level
+ *  @author Laura Pichlmeier 4573524 Gruppe 3b
+ *  @author Sophie Duehn 4577449 Gruppe 3b
+ **/
 public class Level {
-    /**
-     * The constant PLAIN.
-     */
-    public static final char PLAIN = '.';
-    /**
-     * The constant PLAYER_CHAR.
-     */
-    public static final char PLAYER_CHAR = 'P';
-    /**
-     * The constant FOUNTAIN.
-     */
-    public static final char FOUNTAIN = 'O';
-    /**
-     * The constant SMITHY.
-     */
-    public static final char SMITHY = 'T';
-    /**
-     * The constant BATTLE.
-     */
-    public static final char BATTLE = 'B';
-    /**
-     * The constant GOAL.
-     */
-    public static final char GOAL = 'Z';
-    /**
-     * The constant START.
-     */
-    public static final char START = 'S';
-    /**
-     * The constant ATKBONUS.
-     */
-    private static final int ATKBONUS = 10;
-    /**
-     * The Map data.
-     */
+    /** 2D - Array */ 
     private char[][] mapData;
+    /** Spieler Position an der Stelle x */
+    private int playerPositionx;
+    /** Spieler Position an der Stelle y */ 
+    private int playerPositiony;
+    
     /**
-     * The Player x coordinate.
-     */
-    private int playerX;
-    /**
-     * The Player y coordinate.
-     */
-    private int playerY;
-
-    /**
-     * Instantiates a new Level.
-     *
-     * @param mapData the map data
-     */
+     *  Constructor 
+     *  @param mapData mapData
+     **/
     public Level(char[][] mapData) {
-        if (mapData.length < 3 || mapData[0].length < 3) {
-            throw new IllegalArgumentException("Invalid Map Data");
-        }
         this.mapData = mapData;
-        if (!findStart()) {
-            throw new IllegalArgumentException("Invalid Map Data: No starting position");
-        }
-    }
-
-    /**
-     * Random monster.
-     *
-     * @return the monster
-     */
-    private static Monster randomMonster() {
-        Monster[] monsterFarm = {
-            new Monster(),
-            new ResistantMonster(),
-            new WaitingMonster()
-        };
-
-        double bucketSize = 1.0 / monsterFarm.length;
-        double bucket = Math.random() / bucketSize;
-        int selectedMonster = (int) Math.floor(bucket);
-        return monsterFarm[selectedMonster];
-    }
-
-    /**
-     * Find start.
-     *
-     * @return true, wenn die Startposition gefunden wuerde
-     */
-    private boolean findStart() {
-        for (int y = 0; y < mapData.length; y++) {
-            for (int x = 0; x < mapData[0].length; x++) {
-                if (mapData[y][x] == START) {
-                    playerX = x;
-                    playerY = y;
-                    return true;
+        this.playerPositionx = 0;
+        this.playerPositiony = 0;
+        for (int i = 0; i < mapData.length; i++) {
+            for (int j = 0; j < mapData[i].length; j++) {
+                if (mapData[i][j] == 'S') {
+                    this.playerPositionx = i;
+                    this.playerPositiony = j;
                 }
             }
         }
-        return false;
     }
-
+    
     /**
-     * To string.
-     *
-     * @return the string
-     */
+     *  Map String wird erstellt
+     *  @return result result
+     **/
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-        for (int y = 0; y < mapData.length; ++y) {
-            for (int x = 0; x < mapData[0].length; ++x) {
-                if (y == playerY && x == playerX) {
-                    sb.append(PLAYER_CHAR);
+        String result = "";
+        for (int i = 0; i < mapData.length; i++) {
+            for (int j = 0; j < mapData[i].length; j++) {
+                if (playerPositionx == i && playerPositiony == j) {
+                    result = result + "|" + "P";
                 } else {
-                    sb.append(mapData[y][x]);
+                    result = result + "|" + mapData[i][j];
                 }
-            }
-            sb.append("\n");
+            } 
+            result = result + "\n";
         }
-        return sb.toString();
-    }
-
+        result = result.replace(".", " "); 
+        return result; 
+    }     
+    
     /**
-     * Can move.
-     *
-     * @param c the direction
-     *
-     * @return true, wenn die Richtung moeglich ist
-     */
-    public boolean canMove(char c) {
-        switch (c) {
-            case 'n':
-                return canMoveUp();
-            case 's':
-                return canMoveDown();
-            case 'o':
-                return canMoveRight();
-            case 'w':
-                return canMoveLeft();
-            default:
-                return false;
+     *  @return true, wenn Spieler auf Zielfeld ist
+     **/
+    public boolean zielfeld() {
+        if (this.mapData[this.playerPositionx][this.playerPositiony] == 'Z') {
+            return true;
+        } else {
+            return false;
         }
     }
-
+    
     /**
-     * Move void.
-     *
-     * @param c the direction
-     */
-    public void move(char c) {
-        switch (c) {
-            case 'n':
-                moveUp();
-                break;
-            case 's':
-                moveDown();
-                break;
-            case 'o':
-                moveRight();
-                break;
-            case 'w':
-                moveLeft();
-                break;
-        }
+     *  @param x x-Wert
+     *  @param y y-Wert
+     *  @return true, wenn wand im Weg
+     **/
+    public boolean wand(int x, int y) {
+        if (x >= 0 && y >= 0 && x < mapData.length) {
+            if (y <= this.mapData[x].length) {
+                if (this.mapData[x][y] != 'x') {
+                    return false;
+                }
+            } 
+        } 
+        return true;
     }
-
+    
     /**
-     * Is walkable position.
-     *
-     * @param x the x coordinate
-     * @param y the y coordinate
-     *
-     * @return true, wenn das Feld x,y begehbar ist
-     */
-    public boolean isWalkablePosition(int x, int y) {
-        return (y >= 0) && (x >= 0) && (y < mapData.length) && (x < mapData[0].length)
-            && (mapData[y][x] == PLAIN || mapData[y][x] == FOUNTAIN || mapData[y][x] == SMITHY
-            || mapData[y][x] == BATTLE || mapData[y][x] == GOAL || mapData[y][x] == START);
+     *  Schmiede: ATK werden erhoeht
+     *  @param player Player
+     **/
+    public void schmiede(Player player) {
+        player.atkerhoehen(5);
+        System.out.println("Deine ATK wurde um 5 erhoeht.\n");
     }
-
+    
     /**
-     * Can move up.
-     *
-     * @return true, wenn moegliche Bewegung
-     */
-    public boolean canMoveUp() {
-        return isWalkablePosition(playerX, playerY - 1);
+     *  Heilbrunnen: HP und AP werden aufgefuellt
+     *  @param player Player
+     **/
+    public void heilbrunnen(Player player) {
+        player.hperhoehen();
+        player.aperhoehen();
+        System.out.println("Du wurdest vollgeheilt. \n");
     }
-
+    public int buy(Player player,Shop shop,int i){
+    	shop.inventar.delete(player.inventar.getItem(i));
+		
+		return player.getGold() + player.inventar.getItem(i).getValue();
+    	
+    }
+    public int sell(Player player, Shop shop,int i) {
+		return (int) (player.getGold() - (shop.inventar.getItem(i).getValue()*1.3));
+		
+		
+	}
     /**
-     * Can move down.
-     *
-     * @return true, wenn moegliche Bewegung
-     */
-    public boolean canMoveDown() {
-        return isWalkablePosition(playerX, playerY + 1);
+     *  @return playerPositionx
+     **/
+    public int getPlayerPositionx() {
+        return this.playerPositionx;
     }
-
+    
     /**
-     * Can move left.
-     *
-     * @return true, wenn moegliche Bewegung
-     */
-    public boolean canMoveLeft() {
-        return isWalkablePosition(playerX - 1, playerY);
+     *  @return playerPositiony
+     **/
+    public int getPlayerPositiony() {
+        return this.playerPositiony;
     }
-
+    
     /**
-     * Can move right.
-     *
-     * @return true, wenn moegliche Bewegung
-     */
-    public boolean canMoveRight() {
-        return isWalkablePosition(playerX + 1, playerY);
+     *  @param x neue playerPositionx
+     **/
+    public void setPlayerPositionx(int x) {
+        this.playerPositionx = x;
     }
-
+    
     /**
-     * Move up.
-     */
-    public void moveUp() {
-        if (canMoveUp()) {
-            playerY--;
-        }
-    }
-
+     *  @param y neue playerPositiony
+     **/
+    public void setPlayerPositiony(int y) {
+        this.playerPositiony = y;
+    }   
+    
     /**
-     * Move down.
-     */
-    public void moveDown() {
-        if (canMoveDown()) {
-            playerY++;
-        }
+     *  @return Feld
+     **/
+    public char getFeld() {
+        return this.mapData[playerPositionx][playerPositiony];
     }
-
+    
     /**
-     * Move left.
-     */
-    public void moveLeft() {
-        if (canMoveLeft()) {
-            playerX--;
-        }
-    }
-
-    /**
-     * Move right.
-     */
-    public void moveRight() {
-        if (canMoveRight()) {
-            playerX++;
-        }
-    }
-
-    /**
-     * Show prompt.
-     */
-    public void showPrompt() {
-        System.out.println("------------------------------");
-        if (canMoveUp()) {
-            System.out.println("n -> Norden");
-        }
-        if (canMoveDown()) {
-            System.out.println("s -> Sueden");
-        }
-        if (canMoveRight()) {
-            System.out.println("o -> Osten");
-        }
-        if (canMoveLeft()) {
-            System.out.println("w -> Westen");
-        }
-        System.out.println("i -> zeige Inventar");
-        System.out.println("------------------------------");
-        System.out.print("Befehl? ");
-    }
-
-    /**
-     * Gets field.
-     *
-     * @return the field
-     */
-    private char getField() {
-        return mapData[playerY][playerX];
-    }
-
-    /**
-     * Clear field.
-     */
-    private void clearField() {
-        char field = getField();
-        if (field == SMITHY || field == FOUNTAIN || field == BATTLE) {
-            mapData[playerY][playerX] = PLAIN;
-        }
-    }
-
-    /**
-     * Handle current field event.
-     *
-     * @param p the player
-     */
-    public void handleCurrentFieldEvent(Player p) {
-        char field = getField();
-        switch (field) {
-            case Level.SMITHY:
-                p.setAtk(p.getAtk() + ATKBONUS);
-                System.out.printf("Die ATK des Spielers wurde um %d erhoeht.%n", ATKBONUS);
-                break;
-            case Level.FOUNTAIN:
-                p.setHp(p.getMaxHp());
-                System.out.println("Spieler wurde vollstaendig geheilt!");
-                break;
-            case Level.BATTLE:
-                startBattle(p);
-                break;
-            case Level.GOAL:
-                System.out.println("Herzlichen Glueckwunsch! Sie haben gewonnen!");
-                System.exit(0);
-                break;
-        }
-        clearField();
-    }
-
-    /**
-     * Start battle.
-     *
-     * @param p the p
-     */
-    public void startBattle(Player p) {
-        Character m = randomMonster();
-
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("                 Kampf Start                    ");
-        System.out.print(p);
-        System.out.print(m);
-
-        while (true) {
-            System.out.println("------------------------------------------------");
-            System.out.println("Moegliche Aktionen:");
-            System.out.println("1 -> Angriff");
-            System.out.printf("2 -> Item (%d verbleibend)%n", p.getRemainingItemUses());
-            System.out.printf("3 -> Harter Schlag (%d AP, %d%% Selbstschaden)%n",
-                Player.HARD_HIT_COST, Player.HARD_HIT_SELF_DAMAGE_PERCENT);
-            System.out.printf("4 -> Feuerball (%d AP)%n", Player.FIREBALL_COST);
-            System.out.printf("5 -> ATK auswuerfeln (%d AP)%n", Player.REROLL_COST);
-            System.out.println("Welche Aktion?: ");
-            System.out.println("------------------------------------------------");
-            String aktion = sc.nextLine();
-            int playerDamage;
-            switch (aktion) {
-                case "1":
-                    playerDamage = p.attack(m);
-                    if (playerDamage == -1) {
-                        System.out.println("Spieler verfehlt!");
-                    } else {
-                        System.out.printf("Spieler trifft und macht %d Schaden!%n", playerDamage);
-                    }
-                    break;
-                case "2":
-                    if (p.heal()) {
-                        System.out.println("Spieler heilt sich!");
-                    } else {
-                        System.out.println("Nicht genuegend Heiltraenke!");
-                    }
-                    break;
-                case "3":
-                    playerDamage = p.hardHit(m);
-                    if (playerDamage != -1) {
-                        System.out.println("Spieler schlaegt hart zu!");
-                        System.out.printf("Spieler verursacht %d Schaden!%n", playerDamage);
-                        System.out.printf("Spieler verursacht %d Selbstschaden!%n",
-                            (int) (Player.HARD_HIT_SELF_DAMAGE_PERCENT / 100.0 * playerDamage));
-                    } else {
-                        System.out.println("Nicht genuegend AP!");
-                    }
-                    break;
-                case "4":
-                    playerDamage = p.fireball(m);
-                    if (playerDamage != -1) {
-                        System.out.println("Spieler schiesst einen Feuerball!");
-                        System.out.printf("Spieler verursacht %d Schaden!%n", playerDamage);
-                    } else {
-                        System.out.println("Nicht genuegend AP!");
-                    }
-                    break;
-                case "5":
-                    if (p.reroll()) {
-                        System.out.println("ATK neu ausgewuerfelt!");
-                        System.out.print("Neue Statuswerte: ");
-                        System.out.print(p);
-                    } else {
-                        System.out.println("Nicht genuegend AP!");
-                    }
-                    break;
-                default:
-                    System.out.println("Fehlerhafte Aktion!");
-                    continue;
-            }
-
-            if (p.isDefeated()) {
-                System.out.println("Game Over!");
-                System.exit(0);
-            } else if (m.isDefeated()) {
-                System.out.println("Spieler gewinnt!");
-                p.loot(m);
-                break;
-            }
-
-            System.out.print(p);
-            System.out.print(m);
-
-            System.out.println("Monster greift an!");
-            int monsterDamage = m.attack(p);
-            if (monsterDamage == -1) {
-                System.out.println("Monster verfehlt!");
-            } else if (monsterDamage == -2) {
-                System.out.println("Monster tut nichts.");
-            } else {
-                System.out.printf("Monster trifft und macht %d Schaden!%n", monsterDamage);
-            }
-
-            if (p.isDefeated()) {
-                System.out.println("Game Over!");
-                System.exit(0);
-            }
-
-            p.regenerateAp();
-
-            System.out.print(p);
-            System.out.print(m);
-        }
-    }
-
+     *  @param x ersetzt Feld
+     *  @param y ersetzt Feld
+     *  @param z ersetzt bestimmten Buchstaben
+     **/
+    public void replaceFeld(int x, int y, char z) {
+        this.mapData[x][y] = z;
+    }        
 }
